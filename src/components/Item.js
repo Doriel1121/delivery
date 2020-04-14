ee24853dc4cfacacbc1af90c43202e61540da0cb
Doriel aboya <doriel494@gmail.com>
2020-04-14 17:46:54 +0300

    amount add

--- src/components/Item.js
+++ src/components/Item.js
@@ -6,17 +6,36 @@ export default class Item extends Component {
         super(props)
     
         this.state = {
-             
+             status:false,
+        }
+    }
+
+    amoutOf=()=>{
+        if (!this.state.status) {
+            this.setState({status:true})
+        }else{
+            this.setState({status:false})
+        }
+    }
+    showMe=()=>{
+        if (!this.state.status) {
+            return  <div className="eachItem">
+            {this.props.item.picture}<br/>
+                <span className="itemnamestyle">{this.props.item.name}</span><br/>
+                :מחיר לקילו <br/> {this.props.item.price} ש"ח  <br/>
+                <Button onClick={this.amoutOf} variant="contained" color="primary">+</Button>
+        </div>
+        }else{ return <div>
+            <span className="edit"> כמות:</span><br/>
+            <input type="string" placeholder="כמה קילו"/> 
+            </div>
         }
     }
     
     render() {
         return (
             <div className="eachItem">
-                {this.props.item.picture}<br/>
-                    <span className="itemnamestyle">{this.props.item.name}</span><br/>
-                    :מחיר לקילו <br/> {this.props.item.price} ש"ח  <br/>
-                    <Button onClick={this.amoutOf} variant="contained" color="primary">+</Button>
+                    {this.showMe()}
             </div>
         )
     }
