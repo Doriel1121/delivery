d7c6d3dc948ddc22275d59e00ecdf1c4126ce5a9
omer-dayan <omerxdayan1@gmail.com>
2020-04-23 13:56:39 +0100

    RTL

--- src/App.js
+++ src/App.js
@@ -5,22 +5,37 @@ import ManagerPage from './components/ManagerPage';
 import Client from './components/Client.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {HashRouter as Router , Route , Switch} from 'react-router-dom';
+import { create } from 'jss';
+import rtl from 'jss-rtl';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { StylesProvider, jssPreset } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+	direction: 'rtl'
+});
+
+const jss = create({ plugins: [...jssPreset().plugins, rtl()] });
 
 export default class App extends Component {
   render() {
     return (
       <div className="AllApp">
-        <Router>
-        <Client/>
-        <Switch>
-          <Route exact path ="/manager">
-          <ManagerPage/>
-          </Route>
-          <Route exact path="/edit">
-              <EditStorePage />
-          </Route>
-        </Switch>
-        </Router>
+      
+				<ThemeProvider theme={theme}>
+					<StylesProvider jss={jss}>
+            <Router>
+            <Client/>
+              <Switch>
+                <Route exact path ="/manager">
+                <ManagerPage/>
+                </Route>
+                <Route exact path="/edit">
+                    <EditStorePage />
+                </Route>
+              </Switch>
+            </Router>
+					</StylesProvider>
+				</ThemeProvider>
       </div>
     )
   }
